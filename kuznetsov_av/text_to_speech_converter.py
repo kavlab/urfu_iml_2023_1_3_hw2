6feeeab14b8769002ea450fdb01ee85e57167710
Kuznetsov AV <kav.logrus@gmail.com>
2023-11-14 22:49:07 +0400

    text-to-speech module completed

---
+++ kuznetsov_av/text_to_speech_converter.py
@@ -0,0 +1,41 @@
+from transformers import pipeline
+import transformers.pipelines.text_to_audio
+from datasets import load_dataset
+import datasets.arrow_dataset
+import torch
+import numpy as np
+
+
+def load_model() -> transformers.pipelines.text_to_audio.TextToAudioPipeline:
+    """
+    Подгрузка модели преобразования текста в речь
+    :return: class TextToAudioPipeline
+    """
+    return pipeline("text-to-speech", "microsoft/speecht5_tts")
+
+
+def load_speaker_dataset() -> datasets.arrow_dataset.Dataset:
+    """
+    Подгрузка датасета для озвучивания текста
+    :return: class Dataset
+    """
+    return load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+
+
+def text_to_speech(
+        text: str,
+        synthesiser: transformers.pipelines.text_to_audio.TextToAudioPipeline,
+        embeddings_dataset: datasets.arrow_dataset.Dataset
+        ) -> (np.ndarray, int):
+    """
+    Преобразование текста в речь
+    :param text: Текст
+    :param synthesiser: pipeline для озвучивания текста
+    :param embeddings_dataset: dataset для озвучивания текста
+    :return: tuple (audio data, sampling rate)
+    """
+    speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+    speech = synthesiser(text, forward_params={"speaker_embeddings": speaker_embedding})
+
+    return speech['audio'], speech['sampling_rate']
--- run.py
+++ run.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 from mulyavin_aa import langdetector
 from mulyavin_aa import translator
+from kuznetsov_av import text_to_speech_converter
 
 LANG_DETECTOR = "LANG_DETECTOR"
 TRANSLATOR = "TRANSLATOR"
+TEXT_TO_SPEECH = "TEXT_TO_SPEECH"
+SPEAKER_DATASET = "SPEAKER_DATASET"
 
 
 @st.cache_resource
@@ -16,6 +19,8 @@ def load_models() -> dict:
     models = dict()
     models[LANG_DETECTOR] = langdetector.load_text_detection_model()
     models[TRANSLATOR] = translator.load_text_translator_model()
+    models[TEXT_TO_SPEECH] = text_to_speech_converter.load_model()
+    models[SPEAKER_DATASET] = text_to_speech_converter.load_speaker_dataset()
 
     return models
 
@@ -49,7 +54,10 @@ def main_app():
         tab1, tab2, tab3 = st.tabs(['Озвученный текст', 'Таб 2', 'Таб 3'])
         with tab1:
             st.header("Озвученный текст на английском языке")
-            # st.audio()
+            # Преобразование текста в речь
+            audio_data, sampling_rate = text_to_speech_converter.text_to_speech(
+                input_text, models[TEXT_TO_SPEECH], models[SPEAKER_DATASET])
+            st.audio(data=audio_data, sample_rate=sampling_rate)
 
         with tab2:
             st.header("Таб 2")
