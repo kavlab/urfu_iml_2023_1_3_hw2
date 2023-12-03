c137ba2d2b0e1513ee6cabef7b70c6d4ea186a21
Ekaterina Zvereva <evzvereva1@gmail.com>
2023-12-04 00:42:22 +0500

    Added add develop_api_app.py in zvereva_ev

--- zvereva_ev/develop_api_app.py
+++ zvereva_ev/develop_api_app.py
@@ -1,23 +1,8 @@
-from fastapi import FastAPI
-from transformers import pipeline
-
-# Создание объекта FastAPI
-app = FastAPI()
-# Создание классификатора из библиотеки Hugging Face на основе пайплайна с типом "image-to-text"
-classifier = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+from pydantic import BaseModel
 
 
-@app.get("/")
-def root():
+class Url(BaseModel):
     """
-    Приветственное сообщение
+    Формат ответа
     """
-    return {"message": "Follow the link http://127.0.0.1:8000/predict/ and get a description of the image"}
-
-
-@app.get("/predict/")
-async def predict():
-    """
-    Позволяет передать изображение 'image_result.jpg' для получения его описания
-    """
-    return classifier("image_result.jpg")[0]
+    text: str
